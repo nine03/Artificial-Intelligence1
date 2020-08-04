8ff21a709b7f298ecee5d84014816fa4bf7824cc
KIM YEONGJUN <60682087+nine03@users.noreply.github.com>
2020-08-04 10:23:40 +0900

    sources upload
    sources upload

---
+++ sources/三层神经网络与信号传递过程3.py
@@ -0,0 +1,32 @@
+def sigmoid(x): #激活函数
+    return 1/(1+np.exp(-x)) 
+
+def identity_function(x):  #恒等函数
+    return x
+
+def init_network(): #初始化参数
+    network = {}  
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])   
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])   
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
+    return network
+
+def forward(network, x):   #信号传递过程计算
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']  
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+    a1 = np.dot(x, W1) + b1   
+    z1 = sigmoid(a1)    
+    a2 = np.dot(z1, W2) + b2   
+    z2 = sigmoid(a2)  
+    a3 = np.dot(z2, W3) + b3    
+    y = identity_function(a3)
+    return y
+
+network = init_network() 
+x = np.array([1, 0.5]) 
+y = forward(network, x)
+
+print(y)  #[0.31682708 0.69627909] 
